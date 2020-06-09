0b98930c968d663bf4225b2c6872ca3c7a138de2
Stelian Georgiev <koeuni@abv.bg>
2020-06-09 14:16:06 +0300

    Remove duplicates from a sorted list

---
+++ ex1.php
@@ -0,0 +1,15 @@
+<?php
+
+function remove_dup_list($list1) {
+
+    $nums_unique = array_values(array_unique($list1)) ;
+
+    return $nums_unique ;
+
+}
+
+$nums = array(1,1,2,2,3,4,5,5) ;
+
+print_r(remove_dup_list($nums)) ;
+
+?>
