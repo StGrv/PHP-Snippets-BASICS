3be99550a083e623757e2fea58a0dd78602f484e
StGrv <65493257+StGrv@users.noreply.github.com>
2023-01-22 12:58:17 +0200

    Function to validate an email

--- ex2.php
+++ ex2.php
@@ -1,24 +1,17 @@
 <?php
 
-function valid_email($email) {
-
+function valid_email($email) 
+{
     $result = trim($email) ;
-    if (filter_var($result, FILTER_VALIDATE_EMAIL)) {
-
-    return "true" ;
-        
-}
-
-else {
-
-    return "false" ;
     
-}
-
+    if (!filter_var($result, FILTER_VALIDATE_EMAIL)) {
+        return false;  
+    }
+    
+    return true;
 }
 
 echo valid_email("masteradmin@example.com") .'<br>' ;
 echo valid_email("Lest123#example.com") ;
 
-    
 ?>
