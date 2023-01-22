87ec31b1545c0a3429ca72ae4bfb5d776e5d5066
StGrv <65493257+StGrv@users.noreply.github.com>
2023-01-22 13:02:03 +0200

    Try-Catch Blocks

--- ex7.php
+++ ex7.php
@@ -3,23 +3,17 @@
 //Test if the number is 10 or more
 try {
     $num = 5 ;
-    if($num <= 10)  
-{
-        
-//If the exception throws then catch block will display the following error message
-throw new Exception("The number must be 10 or more!") ;
-}
     
-//Executes this line if no error appears.    
-else { echo "The number is: " .$num ; }
-
-}
- 
-//catch the exception from try block
-catch(Exception $e)
-{
-//Print the error message passed from try block
-echo $e->getMessage();
+    if($num <= 10)  {
+        //If the exception throws then catch block will display the following error message
+        throw new Exception("The number must be 10 or more!") ;
+    }
+    
+    //Executes this line if no error appears.    
+    else { echo "The number is: " .$num ; }
+} catch(Exception $e) {
+    //Print the error message passed from try block
+    echo $e->getMessage();
 }
 
 finally {
@@ -27,4 +21,3 @@ finally {
 }
  
 ?>
- 
