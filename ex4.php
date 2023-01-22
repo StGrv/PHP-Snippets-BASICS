a5924417af257dc14fc45eb5684398c09f8a3a56
StGrv <65493257+StGrv@users.noreply.github.com>
2023-01-22 12:59:19 +0200

    Insert a new item in an array in any position

--- ex4.php
+++ ex4.php
@@ -4,23 +4,19 @@ $arr = array('1', '2', '3', '4', '5') ;
 echo 'Original array: ' ."\n" ;
 
 foreach ($arr as $key) {
-
     echo "$key " ;
-
 }
 
 echo "<br>" ;
 
-$inserted = '$' ;
-array_splice($arr, 3, 0,  $inserted) ;
+$inserted = '$ ;
+
+array_splice($arr, 3, 0,  $inserted);
 
 echo " \n After inserting '$' the array is : "."\n" ;
 foreach ($arr as $key) {
-
     echo "$key " ;
     echo "\n" ;
-
 }
-
 
 ?>
