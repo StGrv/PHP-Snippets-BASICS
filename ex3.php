6de3428dc5db0a1ac4072aa8845a10e2465abbda
StGrv <65493257+StGrv@users.noreply.github.com>
2023-01-22 12:58:53 +0200

    Get the first element of an associative array

--- ex3.php
+++ ex3.php
@@ -1,16 +1,12 @@
 <?php
 
-$Arr = array( 
-    
+$arr = array( 
     10 => 'white' ,
     20  => 'green' ,
     30 => 'red'  
-    
 ) ;
 
-$firstkey = array_key_first($Arr) ;
+$firstkey = array_key_first($arr);
 echo $firstkey ;
-
-
 
 ?>
